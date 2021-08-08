179cd1089ffaf15fa78ce5b83c942317964981eb
Oliver Tacke <o.tacke@posteo.de>
2021-08-08 15:28:35 +0200

    Modernize build chain

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
 const path = require('path');
-const webpack = require('webpack');
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 
 const nodeEnv = process.env.NODE_ENV || 'development';
-const isDev = (nodeEnv !== 'production');
+const isProd = (nodeEnv === 'production');
 
-const config = {
-  mode: 'development',
+module.exports = {
+  mode: nodeEnv,
   optimization: {
+    minimize: isProd,
     minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: isDev
+      new TerserPlugin({
+        terserOptions: {
+          compress:{
+            drop_console: true,
+          }
+        }
       }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
@@ -31,34 +31,43 @@ const config = {
     filename: 'h5p-mini-course.js',
     path: path.resolve(__dirname, 'dist')
   },
+  target: ['web', 'es5'], // Damn you, IE11!
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
+        exclude: /node_modules/,
+        loader: 'babel-loader'
       },
       {
-        test: /\.css$/,
+        test: /\.(s[ac]ss|css)$/,
         use: [
-          MiniCssExtractPlugin.loader, 'css-loader'
-        ],
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ''
+            }
+          },
+          { loader: "css-loader" },
+          {
+            loader: "sass-loader"
+          }
+        ]
       },
       {
-        test: /\.svg$/,
-        loader: 'url-loader'
+        test: /\.svg|\.jpg|\.png$/,
+        include: path.join(__dirname, 'src/images'),
+        type: 'asset/resource'
+      },
+      {
+        test: /\.woff$/,
+        include: path.join(__dirname, 'src/fonts'),
+        type: 'asset/resource'
       }
     ]
   },
   stats: {
     colors: true
-  }
+  },
+  devtool: (isProd) ? undefined : 'eval-cheap-module-source-map'
 };
-
-if (isDev) {
-  config.devtool = 'inline-source-map';
-}
-
-module.exports = config;
